c476728bf533bfb8c2908a139c02d5f294dee502
jithu2023 <jithumon.j@kalvium.community>
2025-03-30 18:12:43 +0530

    project setup and loginpage

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import Login from '../components/Login'
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Login/>
+    </View>
+  );
+}
